12f64b4f434be8610299b15e091e328f21a6ad24
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-04-18 21:52:07 -0500

    Add Player update function and Race functions

--- HW_8/utilities.py
+++ HW_8/utilities.py
@@ -1,10 +1,26 @@
 from random import randint, uniform
 
 class Player:
-    def __init__(self) -> None:
+    def __init__(self, name) -> None:
+        self.name = f"{name}"
         self.speed = randint(1, 10)
         self.max_distance = uniform(150, 500)
+
+        self.can_run = True
+        self.x = 0
+
+    def update(self):
+        if (self.can_run and self.x < self.max_distance):
+            self.x += self.speed
+        else:
+            self.can_run = False
 
 class Race:
     def __init__(self, distance) -> None:
         self.distance = distance
+
+    def start_race(self, racers):
+        pass
+
+    def determine_winner(self):
+        pass
