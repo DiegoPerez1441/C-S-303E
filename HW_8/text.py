c6ecae17aacc8aea655a4e8efb9dbc072f4b61cd
DiegoPerez1441 <devdiegoperez@gmail.com>
2022-04-23 15:55:01 -0500

    Add function annotations and _Color type alias

--- HW_8/text.py
+++ HW_8/text.py
@@ -1,14 +1,16 @@
-from typing import Union
+from typing import Union, Tuple
 
 import pygame
 from pygame.locals import *
+
+_Color = Tuple[int, int, int]
 
 pygame.init()
 
 sys_font = pygame.font.get_default_font()
 font = pygame.font.SysFont(None, 24)
 
-def render_text(surface: pygame.surface.Surface, text: str, x: Union[int, float], y: Union[int, float], color) -> None:
+def render_text(surface: pygame.surface.Surface, text: str, x: Union[int, float], y: Union[int, float], color: _Color) -> None:
     img = font.render(text, True, color)
     rect = img.get_rect()
     surface.blit(img, (x, y))
